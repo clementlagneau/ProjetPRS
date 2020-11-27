11ddafc70e43ddd03a6aa3764dae369bcfde2fc1
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-11-28 00:22:45 +0100

    Test du timeout implementation

--- python/benchmark2.py
+++ python/benchmark2.py
@@ -7,7 +7,7 @@ def f(k):
     os.system("python3 server_test.py 1234")
 
 def g(k):
-    subprocess.call(["/bin/bash","-c","(time ./client1 134.214.202.236 1234 test.txt "+str(k)+" >> log_"+str(k)+".txt 2>&1"])
+    subprocess.call(["/bin/bash","-c","(time ./client1 134.214.202.236 1234 test.txt "+str(k)+") >> log_"+str(k)+".txt 2>&1"])
 
 if __name__ == '__main__':
     for k in range(5,1000,5):
