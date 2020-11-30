0e028cb44bb42ce91b21a161615297b13caf38ba
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-11-30 17:36:51 +0100

    Gestion des ACKs

--- python/server_client1_test.py
+++ python/server_client1_test.py
@@ -14,8 +14,8 @@ def main():
       print("Utilisation : /server Nport")
       return(-1)
 
-    timeout = 0.001
-    taille_fenetre = 25
+    timeout = 0.002
+    taille_fenetre = 20
     dernier_ack = 0
 
     # Creation des sockets
