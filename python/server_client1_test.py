39fdf5b09072a4d8de54c10c39a1333619e859eb
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-11-30 17:12:38 +0100

    Gestion des ACKs

--- python/server_client1_test.py
+++ python/server_client1_test.py
@@ -80,8 +80,8 @@ def main():
                                   len(file_cut[k-1]), " bits")
                     if change:
                         fenetre_haut = min(dernier_ack+1+taille_fenetre,tot_seq)
-                        print("Send little slice "+str(fenetre_haut-delta+1)+"to"+str(fenetre_haut))
-                        for k in range(fenetre_haut-delta+1,fenetre_haut+1):
+                        print("Send little slice "+str(dernier_ack+1+taille_fenetre-delta+1)+"to"+str(fenetre_haut))
+                        for k in range(dernier_ack+1+taille_fenetre-delta,fenetre_haut+1):
                             sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
                             print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
                                   len(file_cut[k-1]), " bits")
