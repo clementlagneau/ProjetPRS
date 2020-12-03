0189d742a52c8bd12322d9de5482f28f884099d8
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-03 21:35:23 +0100

    Gestion des ACKs

--- python/server_client1_test.py
+++ python/server_client1_test.py
@@ -122,7 +122,7 @@ def main():
                         ack_ignore += 1
                         ack_ignore_debug +=1
             except socket.error:
-                timeout = 0.008
+                timeout = 0.02
                 taille_fenetre = 60
                 debut = True
                 change = False
