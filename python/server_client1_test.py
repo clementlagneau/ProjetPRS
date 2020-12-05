a4d04b04ed01c7a6ea33eac787f7900112d2e0b0
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-05 22:04:55 +0100

    Gestion des ACKs

--- python/server_client1_test.py
+++ python/server_client1_test.py
@@ -95,7 +95,6 @@ def main():
                     sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
                     print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
                           len(file_cut[k-1]), " bits")
-                time.sleep(0.0001)
             if change:
                 #Cas fenetre glissante
                 change = False
@@ -105,7 +104,6 @@ def main():
                     sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
                     print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
                           len(file_cut[k-1]), " bits")
-                time.sleep(0.0001)
             print("Wait ACK")
             data, address_client = sock_data.recvfrom(SIZE_BUFFER)
             if data.decode()[:3] == "ACK":
@@ -121,7 +119,7 @@ def main():
                     fenetre_continue = max(fenetre_continue+1, 70)
                     taille_fenetre += 1
                 else:
-                    if ack_ignore > 2:
+                    if ack_ignore > 10:
                         #debut = True
                         time.sleep(0.0005)
                         print("Retransmit all")
@@ -131,8 +129,8 @@ def main():
                         ack_ignore +=1
                     ack_ignore_debug +=1
         except socket.error:
-            timeout = 0.006
-            taille_fenetre = 30
+            timeout = 0.008
+            taille_fenetre = 20
             debut = True
             change = False
             print("Retransmit")
