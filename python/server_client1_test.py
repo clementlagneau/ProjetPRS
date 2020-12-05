6308b20507590403c5db949d7a141444305db710
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-05 21:45:26 +0100

    Gestion des ACKs

--- python/server_client1_test.py
+++ python/server_client1_test.py
@@ -72,70 +72,69 @@ def main():
         file_cut = []
         for k in range(tot_seq):
             file_cut.append(copy.deepcopy((file.read(SIZE_BUFFER))))
-        #On envoie le fichier petit a petit
-        print("len file_cut",len(file_cut))
-        #Il faut qu'on fasse les fenetres ici
-        last_ack = False
-        change = False
-        debut = True
-        while (not last_ack):
-            sock_data.settimeout(timeout)
-            try:
-                if dernier_ack == tot_seq:
-                    #On a recu le dernier ACK
-                    last_ack = True
-                    break
-                if debut:
-                    #Cas ou on revoie tout
-                    ack_ignore = 0
-                    debut = False
-                    fenetre_haut = min(dernier_ack+1+taille_fenetre,tot_seq)
-                    print("Send full slice "+str(dernier_ack+1)+"to"+str(fenetre_haut))
-                    for k in range(dernier_ack+1,fenetre_haut+1):
-                        sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
-                        print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
-                              len(file_cut[k-1]), " bits")
-                    time.sleep(0.0005)
-                if change:
-                    #Cas fenetre glissante
-                    fenetre_haut = min(dernier_ack+1+taille_fenetre+1,tot_seq)
-                    print("Send little slice "+str(dernier_ack+1+taille_fenetre+1-delta)+"to"+str(fenetre_haut))
-                    for k in range(dernier_ack+1+taille_fenetre-delta,fenetre_haut+1):
-                        sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
-                        print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
-                              len(file_cut[k-1]), " bits")
-                    time.sleep(0.0002)
-                print("Wait ACK")
-                data, address_client = sock_data.recvfrom(SIZE_BUFFER)
-                if data.decode()[:3] == "ACK":
-                    print("Received "+data.decode())
-                    recu = int(data.decode()[3:9])
-                    if dernier_ack < recu:
-                        #timeout = 0.9999 * timeout + 0.0005
-                        #taille_fenetre = max(2 * taille_fenetre + 5 , 300)
-                        print("ACK > last one")
-                        change = True
-                        delta = min(recu - dernier_ack,taille_fenetre)
-                        dernier_ack = recu
-                        fenetre_continue +=1
-                        timeout = 0.01
+    #On envoie le fichier petit a petit
+    print("len file_cut",len(file_cut))
+    #Il faut qu'on fasse les fenetres ici
+    last_ack = False
+    change = False
+    debut = True
+    while (not last_ack):
+        sock_data.settimeout(timeout)
+        try:
+            if dernier_ack == tot_seq:
+                #On a recu le dernier ACK
+                last_ack = True
+                break
+            if debut:
+                #Cas ou on revoie tout
+                ack_ignore = 0
+                debut = False
+                fenetre_haut = min(dernier_ack+1+taille_fenetre,tot_seq)
+                print("Send full slice "+str(dernier_ack+1)+"to"+str(fenetre_haut))
+                for k in range(dernier_ack+1,fenetre_haut+1):
+                    sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
+                    print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
+                          len(file_cut[k-1]), " bits")
+                time.sleep(0.0001)
+            if change:
+                #Cas fenetre glissante
+                fenetre_haut = min(dernier_ack+1+taille_fenetre+1,tot_seq)
+                print("Send little slice "+str(dernier_ack+1+taille_fenetre+1-delta)+"to"+str(fenetre_haut))
+                for k in range(dernier_ack+1+taille_fenetre-delta,fenetre_haut+1):
+                    sock_data.sendto((bytes(str(k).zfill(6),'utf-8'))+file_cut[k-1], address_client)
+                    print("Send slice " + str(k) + " of total " + str(tot_seq) + " of ",
+                          len(file_cut[k-1]), " bits")
+                time.sleep(0.0001)
+            print("Wait ACK")
+            data, address_client = sock_data.recvfrom(SIZE_BUFFER)
+            if data.decode()[:3] == "ACK":
+                print("Received "+data.decode())
+                recu = int(data.decode()[3:9])
+                if dernier_ack < recu:
+                    #timeout = 0.9999 * timeout + 0.0005
+                    #taille_fenetre = max(2 * taille_fenetre + 5 , 300)
+                    print("ACK > last one")
+                    change = True
+                    delta = min(recu - dernier_ack,taille_fenetre)
+                    dernier_ack = recu
+                    fenetre_continue +=1
+                else:
+                    if ack_ignore > 2:
+                        #debut = True
+                        time.sleep(0.0005)
+                        print("Retransmit all")
+                        ack_ignore = 0
                     else:
-                        if ack_ignore > 2:
-                            #debut = True
-                            time.sleep(0.0005)
-                            print("Retransmit all")
-                            ack_ignore = 0
-                        else:
-                            change = False
-                            ack_ignore +=1
-                        ack_ignore_debug +=1
-            except socket.error:
-                timeout = 0.008
-                taille_fenetre = 100
-                debut = True
-                change = False
-                print("Retransmit")
-                retransmission +=1
+                        change = False
+                        ack_ignore +=1
+                    ack_ignore_debug +=1
+        except socket.error:
+            timeout = 0.008
+            taille_fenetre = 100
+            debut = True
+            change = False
+            print("Retransmit")
+            retransmission +=1
     print("Send FIN")
     time.sleep(0.005)
     sock_data.sendto("FIN".encode(), address_client)
