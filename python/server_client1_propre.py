83c02c8dcf02e31b5aaa7dc0dbb3b2abcd07f5f0
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-08 22:10:12 +0100

    Gestion des ACKs

--- python/server_client1_propre.py
+++ python/server_client1_propre.py
@@ -31,7 +31,7 @@ def main():
     timeout = 0.03
     rtt = 0.02
     taille_fenetre_init = 50
-    aug_taille_fenetre = 20
+    aug_taille_fenetre = 10
     taille_fenetre = taille_fenetre_init
     dernier_ack = 0
     nombre_client = 3000
