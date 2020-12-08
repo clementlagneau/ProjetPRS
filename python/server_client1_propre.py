9a213b1297b35c164ab34b41e741e6448b4b9294
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-08 21:15:15 +0100

    Gestion des ACKs

--- python/server_client1_propre.py
+++ python/server_client1_propre.py
@@ -30,9 +30,9 @@ def main():
     #Definitions des variables globales
     timeout = 0.03
     rtt = 0.02
-    coeff_rtt = 1.2
+    coeff_rtt = 1.4
     taille_fenetre_init = 30
-    aug_taille_fenetre = 3
+    aug_taille_fenetre = 10
     taille_fenetre = taille_fenetre_init
     dernier_ack = 0
     nombre_client = 3000
@@ -92,7 +92,7 @@ def main():
         for j in range(k, n + 1):
             sock_data.sendto((bytes(str(j).zfill(6), 'utf-8')) + file_cut[j - 1], address_client)
             time_file_cut[j] = time.time()
-            print("Send slice " + str(j) + " of total " + str(tot_seq))
+            #print("Send slice " + str(j) + " of total " + str(tot_seq))
         return(n)
 
     #On recupere le fichier
