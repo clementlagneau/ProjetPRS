bada7a00d13833158f3e6560dfa50b726e1112bb
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-08 20:40:56 +0100

    Gestion des ACKs

--- python/server_client1_propre.py
+++ python/server_client1_propre.py
@@ -146,10 +146,10 @@ def main():
                     delta = min(recu - dernier_ack, taille_fenetre)
                     dernier_ack = recu
                     fenetre_continue += 1
-                elif ack_ignore > 4 :
+                elif ack_ignore > 10 :
                     debut = True
                     dernier_ack = recu
-                    time.sleep(0.05)
+                    time.sleep(coeff_rtt*rtt)
                     ack_ignore = 0
                     ack_ignore_debug += 1
                 else:
