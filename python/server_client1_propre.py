c71cdc28a6dbd5e1c9781f26f5649cae00a943d8
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-09 15:00:50 +0100

    Gestion des ACKs

--- python/server_client1_propre.py
+++ python/server_client1_propre.py
@@ -93,8 +93,9 @@ def main():
     def sendkton(k,n):
         #print("Send slice " + str(k) + "to" + str(n))
         for j in range(k, n + 1):
-            sock_data.sendto((bytes(str(j).zfill(6), 'utf-8')) + file_cut[j - 1], address_client)
-            time_file_cut[j] = time.time()
+            for _ in range(2):
+                sock_data.sendto((bytes(str(j).zfill(6), 'utf-8')) + file_cut[j - 1], address_client)
+                time_file_cut[j] = time.time()
             #print("Send slice " + str(j) + " of total " + str(tot_seq))
         return(n)
 
