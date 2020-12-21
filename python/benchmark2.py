926b9c31454fa55f0e15b92f2ff7ec8f911c9e12
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-21 01:23:55 +0100

    Test de parametres automatiquement

--- python/benchmark2.py
+++ python/benchmark2.py
@@ -5,14 +5,20 @@ import time
 
 def f(k):
     b = str(k)
-    os.system("python3 server_test.py 1234 "+b)
+    os.system("python3 server_client1_param.py 1234 "+b)
 
 def g(a):
     b = str(a)
-    subprocess.call(["/bin/bash","-c","(time ./client1 134.214.202.236 1234 chat.jpg) 2> log_"+b+".txt"])
+    subprocess.call(["/bin/bash","-c","(time ./client1 134.214.202.236 1234 out.txt 0) 2> log_"+b+".txt"])
+
+def temps(path):
+    with open(path,'r') as file:
+        res = file.readlines()[1].split('\t')[1].split('m')
+        sec = int(res[0]) * 60 + float(res[1].replace('s\n','').replace(',','.'))
+    return(sec)
 
 if __name__ == '__main__':
-    for k in range(5,1000,5):
+    for k in range(1,100,1):
         print("k vaut",k)
         p = Process(target=f, args=(k,))
         h = Process(target=g, args=(k,))
@@ -21,3 +27,10 @@ if __name__ == '__main__':
         h.start()
         p.join()
         h.join()
+
+    with open('res.txt', 'a') as file:
+        file.writelines("k" + ";" + "temps")
+    for k in range(1,100,1):
+        t = temps("log_"+str(k)+".txt")
+        with open('res.txt','a') as file:
+            file.writelines(k+";"+str(t))
