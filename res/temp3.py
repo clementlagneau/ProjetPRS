944ea893477e14895c921c84cb64ffd3f08b9d9c
clementlagneau <clement.lagn@gmail.com>
2021-01-06 21:02:20 +0100

    preparation du rendu

--- res/temp3.py
+++ res/temp3.py
@@ -4,6 +4,7 @@ max_3 = 1000
 
 with open('res.txt','r') as file:
     for line in file.readlines():
+        t = line.split(';')
         if float(t[2].replace("\n","")) < max_3:
             max_1 = float(t[0])
             max_2 = float(t[1])
